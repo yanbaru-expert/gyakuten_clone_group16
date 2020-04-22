1f35fbad6977164f5ce25987d255210b00859f66
tanahashiyasuhiro <tana.yasu1228@gmail.com>
2020-04-22 16:49:17 +0900

    ログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
  root to: 'movies#index'
+ resources :movies
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,3 @@
+User.create!(email: 'test@example.com', password: 'password') 
+
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
