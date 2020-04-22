11c981e2cb6bbb71cfb201bd94923c5646a13f8c
tanahashiyasuhiro <tana.yasu1228@gmail.com>
2020-04-23 06:56:57 +0900

    ログイン機能の実装を修正しました

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,7 @@
 class MoviesController < ApplicationController
+before_action :authenticate_user!
+
   def index
+    @movies = Movie.all unless user_signed_in?
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,3 @@
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+User.create!(email: 'test@example.com', password: 'password') 
+
+# AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
