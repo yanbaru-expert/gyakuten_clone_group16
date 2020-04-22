0c032c73eb4726fe57c9e1d6990f811c3c2313f0
mk_rutile <mk1988senna.startline@gmail.com>
2020-04-22 14:14:01 +0900

    import.rbを利用してaws_text_data.csvをaws_textsにインポート

--- app/models/aws_text.rb
+++ app/models/aws_text.rb
@@ -1,2 +1,19 @@
 class AwsText < ApplicationRecord
+
+  def self.import(path:)
+    list = []
+    CSV.foreach(pash, headers: true) do |row|
+      list << {
+        title: row["title"],
+        content: row["content"]
+      }
+    end
+    puts "インポート処理を開始"
+    begin
+      AwsText.create!(list)
+      puts "インポート完了!!"
+    rescue ActiveModel::UnknownAttributeError => invalid
+      puts "インポートに失敗：UnknownAttributeError"
+    end
+  end
 end
