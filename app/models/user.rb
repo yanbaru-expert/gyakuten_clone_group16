21e42a3bb48d92f52c944d0e1ef090301883737a
tanahashiyasuhiro <tana.yasu1228@gmail.com>
2020-04-23 16:46:59 +0900

    ログイン機能の実装を修正しました

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,6 @@
 class User < ApplicationRecord
-<<<<<<< HEAD
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-=======
-
->>>>>>> 3bd0ba6d70e1e5a2327bca98befb8c441547bc49
 end
