01da819983570cd6b063970f43c5239f1a3b8f5c
knhs <knhskunibo@gmail.com>
2020-04-29 14:15:57 +0900

    質問一覧ページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root to: 'movies#index'
+
+  get 'questions', to: 'questions#index'
 end
