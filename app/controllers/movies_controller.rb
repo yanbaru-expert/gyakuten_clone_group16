5f399ad87c8bd3c38caef86f3fbe53d96a77000e
Tako <takoforme@gmail.com>
2020-05-13 11:54:12 +0900

    .offset_valueを使用して書き換え

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,6 +3,5 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.page(params[:page]).per(PER)
-    @movie_level = Movie.movie_level(params[:page])
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,5 @@
 class Movie < ApplicationRecord
     validates :title, presence: true
     validates :url, presence: true
-    def self.movie_level(page)
-        (self.page(page).current_page - 1) * 10
-    end
+    
 end
