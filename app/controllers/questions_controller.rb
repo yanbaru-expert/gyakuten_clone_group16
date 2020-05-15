6acc5aaa3630ea5be2bfc410ca456db4dacc5e2b
knhs <knhskunibo@gmail.com>
2020-05-15 16:52:59 +0900

    質問投稿機能を実装

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,16 @@ class QuestionsController < ApplicationController
     def index
         @questions = Question.all.order("id DESC")
     end
+
+    def create
+        Question.create(question_params)
+    end
+
+    
+    private
+
+    def question_params
+        params.permit(:title, :detail)
+    end
+
 end
