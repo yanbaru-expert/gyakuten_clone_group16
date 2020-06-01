138d4918a8e1c8ee4bacff1a4f2d8b7f167b16ff
Tako <takoforme@gmail.com>
2020-06-01 19:57:45 +0900

    AWS, ライブコーディングのルーティングにonlyを追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root to: 'movies#index'
-  resources :aws_texts
+  resources :aws_texts, only: [:index, :show]
   resources :questions
-  resources :live_codings
+  resources :live_codings, only: [:index]
 end
