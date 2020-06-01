690c593c7132ee7400c208a477d07091a45ccda4
Tako <takoforme@gmail.com>
2020-06-01 18:13:02 +0900

    コントローラーを修正

--- app/controllers/live_codings_controller.rb
+++ app/controllers/live_codings_controller.rb
@@ -1,7 +1,6 @@
 class LiveCodingsController < ApplicationController
-  PER = 10
 
   def index
-    @live_codings = LiveCoding.page(params[:page]).per(PER)
+    @live_codings = LiveCoding.all
   end
 end
