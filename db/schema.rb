34ca2602747f0af8b5d0fce43e3b7f19a74dd9ee
Tako <takoforme@gmail.com>
2020-06-02 00:43:45 +0900

    csvインポート機能をRakeタスクに追記

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_213333) do
+ActiveRecord::Schema.define(version: 2020_06_01_153915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,13 @@ ActiveRecord::Schema.define(version: 2020_05_28_213333) do
   end
 
   create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.string "url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "php_lectures", force: :cascade do |t|
     t.string "title"
     t.string "url"
     t.datetime "created_at", precision: 6, null: false
--- lib/tasks/import_csv.rake
+++ lib/tasks/import_csv.rake
@@ -66,6 +66,19 @@ namespace :import_csv do
    end
   end
 
+  task php_lectures: :environment do
+
+    list = Import.csv_data(path: 'db/csv_data/php_lecture_data.csv')
+
+   puts "インポート処理を開始"
+   begin
+    PhpLecture.create!(list)
+     puts "インポート完了!!"
+   rescue ActiveModel::UnknownAttributeError => invalid
+     puts "インポートに失敗：UnknownAttributeError"
+   end
+  end
+
 end
 
 
