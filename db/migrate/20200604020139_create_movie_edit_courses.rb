75be8222f934032da0c544f0dc8c194315f09312
tanahashiyasuhiro <tana.yasu1228@gmail.com>
2020-06-04 11:08:17 +0900

    テーブルの作成

--- db/migrate/20200604020139_create_movie_edit_courses.rb
+++ db/migrate/20200604020139_create_movie_edit_courses.rb
@@ -1,7 +1,8 @@
 class CreateMovieEditCourses < ActiveRecord::Migration[6.0]
   def change
     create_table :movie_edit_courses do |t|
-
+      t.string :title
+      t.string :url
       t.timestamps
     end
   end
