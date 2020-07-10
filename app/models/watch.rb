a58832adbb30a987fe4c59aa9292c9db1acf1c3b
Tako <takoforme@gmail.com>
2020-07-10 23:49:14 +0900

    Watchモデルにアソシーションとバリデーションを追記

--- app/models/watch.rb
+++ app/models/watch.rb
@@ -1,2 +1,6 @@
 class Watch < ApplicationRecord
+  belongs_to :user
+  belongs_to :movie
+  validates :user_id, presence: true, uniqueness: { scope: :movie_id }
+  validates :movie_id, presence: true
 end
